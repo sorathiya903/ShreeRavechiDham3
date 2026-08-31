51139bf621cd8a273346d47c32ee8f5a3f04f887
Aditya <adityasorathiya.4112@gmail.com>
2026-08-31 10:39:52 +0530

    Add events data for Ganesh Chaturthi and Navratri

---
+++ events.js
@@ -0,0 +1,20 @@
+const events = [
+    {
+        id: 1,
+        title: "Ganesh Chaturthi",
+        date: "27 August 2026",
+        time: "7:00 PM",
+        location: "Ravachi Dham 3",
+        image: "/static/images/ganesh-chaturthi.jpg",
+        description: "Community celebration at Ravachi Dham 3."
+    },
+    {
+        id: 2,
+        title: "Navratri Celebration",
+        date: "October 2026",
+        time: "8:00 PM",
+        location: "Society Ground",
+        image: "/static/images/navratri.jpg",
+        description: "Join the community for Navratri celebrations."
+    }
+];
