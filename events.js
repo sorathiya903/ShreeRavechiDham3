354fce2afa7673c6fa10b285cdd46c80405850ac
Aditya <adityasorathiya.4112@gmail.com>
2026-08-31 17:47:12 +0530

    Update event details for Janmasthami and Ganesh Chaturthi

--- events.js
+++ events.js
@@ -1,8 +1,17 @@
 const events = [
     {
         id: 1,
+        title: "Janmasthami",
+        date: "4 September 2026",
+        time: "11:00 AM",
+        location: "Ravachi Dham 3",
+        image: "/static/images/janmasthami.jpg",
+        description: "Janmasthami Celebration at Ravachi Dham 3."
+    },
+    {
+        id: 1,
         title: "Ganesh Chaturthi",
-        date: "27 August 2026",
+        date: "14 September 2026",
         time: "7:00 PM",
         location: "Ravachi Dham 3",
         image: "/static/images/ganesh-chaturthi.jpg",
